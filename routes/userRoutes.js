d308a9df1558b45ec6bfbc3678f9f6bd0e86c4a8
Nuruddin Kawsar <66555208+kawsar-95@users.noreply.github.com>
2021-12-07 22:55:15 +0600

    userApi done

---
+++ routes/userRoutes.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const router = express.Router()
+
+const { userSchema } = require("../model/userSchema.js")
+
+const users = [
+  { id: 1, first_name: 'Jahid', last_name: 'Hossain', age: 23 },
+  { id: 2, first_name: 'Abdur', last_name: 'Rahman', age: 25 },
+];
+
+const validateUser = async user => {
+  try {
+    await userSchema.validate(user); // validateUser gives me null when there is no error
+    return null;
+  }
+  catch (err) {
+    return err.errors[0];
+  }
+};
+
+router.get("/", function (req, res) {
+  res.send(users);
+});
+
+router.get("/:id", function (req, res) {
+  const id = req.params.id;
+  const user = users.find(user => user.id === parseInt(id));
+  if (!user) return res.status(404).send("User not found");
+  res.send(user);
+});
+
+router.post("/", async function (req, res) {
+  const { first_name, last_name } = req.body;
+  try {
+    const err = await validateUser({ first_name, last_name });
+
+    if (err) return res.status(400).send(err);
+
+    const user = {
+      id: users.length + 1,
+      first_name,
+      last_name
+    }
+    users.push(user);
+
+    res.status(201).send(user);
+  }
+  catch (err) {
+    // return res.status(404).send(err.errors[0]);
+    console.log()
+  }
+});
+
+router.put("/:id", function (req, res) {
+  const id = req.params.id;
+  const { first_name, last_name } = req.body;
+
+  const user = users.find((user) => user.id == id);
+
+  if (!user) return res.status(404).send("User not found");
+
+  user.first_name = first_name;
+  user.last_name = last_name;
+
+  res.send(user);
+});
+
+router.delete("/:id", function (req, res) {
+  const id = req.params.id;
+  const user = users.find((user) => user.id == id);
+
+  if (!user) return res.status(404).send("User not found");
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+
+  res.json(user);
+});
+
+router.patch("/:id", function (req, res) {
+  const id = req.params.id;
+  const { first_name, last_name } = req.body;
+
+  const user = users.find((user) => user.id == id);
+
+  if (!user) return res.status(404).send("User not found");
+
+  if (first_name) user.first_name = first_name;
+  if (last_name) user.last_name = last_name;
+
+  res.json(user);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,79 +1,11 @@
 const express = require("express");
 const app = express();
 
-const { userSchema } = require("./userSchema")
-
+const userRouter = require("./routes/userRoutes.js")
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("This is homepage");
-});
-
-const users = [
-  { id: 1, first_name: "Farhan", last_name: "Sadek" },
-  { id: 2, first_name: "Sabbir", last_name: "Ahmed" },
-  { id: 3, first_name: "Rafsan", last_name: "Mahmud" },
-];
-
-app.get("/api/users", function (req, res) {
-  res.send(users);
-});
-
-app.get("/api/users/:id", function (req, res) {
-  const id = req.params.id;
-  const user = users.find(user => user.id === parseInt(id));
-  if (!user) return res.status(404).send("User not found");
-  res.send(user);
-});
-
-app.post("/api/users", function (req, res) {
-  const id = users.length + 1;
-  const { first_name, last_name } = req.body;
-  users.push({ id, first_name, last_name });
-  const user = users.find((user) => user.id == id);
-  res.json(user);
-});
-
-app.put("/api/users/:id", function (req, res) {
-  const id = req.params.id;
-  const { first_name, last_name } = req.body;
-
-  const user = users.find((user) => user.id == id);
-
-  if (!user) return res.status(404).send("User not found");
-
-  user.first_name = first_name;
-  user.last_name = last_name;
-
-  res.send(user);
-});
-
-app.delete("/api/users/:id", function (req, res) {
-  const id = req.params.id;
-  const user = users.find((user) => user.id == id);
-
-  if (!user) return res.status(404).send("User not found");
-
-  const index = users.indexOf(user);
-  users.splice(index, 1);
-
-  res.json(user);
-});
-
-app.patch("/api/users/:id", function (req, res) {
-  const id = req.params.id;
-  const { first_name, last_name } = req.body;
-
-  const user = users.find((user) => user.id == id);
-
-  if (!user) return res.status(404).send("User not found");
-
-  if (first_name) user.first_name = first_name;
-  if (last_name) user.last_name = last_name;
-
-  res.json(user);
-});
+app.use('/api/users', userRouter)
 
 app.listen(3000, () => {
   console.log('Server Running');
