32a15e0d639438354164b1bfda206c3f15c9732d
Nuruddin Kawsar <66555208+kawsar-95@users.noreply.github.com>
2021-12-24 20:34:56 +0600

    routes refactored and middleware added

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const controller = require('../controller/product.controller');
 const router = express.Router();
+const validate = require('../middleware/validate')
+const { productUploadSchema, productUpdateSchema } = require('../schema/product.schema')
 
 router.get('/api/products/', controller.getProducts);
 router.get('/api/products/:id', controller.getProduct);
-router.post('/api/products/', controller.postProduct);
-router.put('/api/products/:id', controller.putProduct);
-router.patch('/api/products/:id', controller.patchProduct);
+router.post('/api/products/', validate(productUploadSchema), controller.postProduct);
+router.put('/api/products/:id', validate(productUpdateSchema), controller.putProduct);
+router.patch('/api/products/:id', validate(productUpdateSchema), controller.patchProduct);
 router.delete('/api/products/:id', controller.deleteProduct);
 
 module.exports = router;
